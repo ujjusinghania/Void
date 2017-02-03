f46fa73850e4ec08f422d6c32ba3f03ddb04509e
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2017-02-02 19:20:28 -0500

    Initial Commit
    - Created the Void project.
    - Added ViewControllers for user inputs.

---
+++ Void/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Void
+//
+//  Created by Ujjwal Singhania on 2/2/17.
+//  Copyright © 2017 Ujjwal Singhania. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
