6926d51c1312096476336c921498d90f7daa40c7
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2017-02-11 23:32:24 -0500

    02/11/2017 - 11:31PM
    - Added the icons for the application.
    - Added the splash screen.
    - Added the nextView button and label to the VoidViewController.
    - Fixed AutoLayout for some views.
    - Miscellaneous fixes to user interface.
    - Numerous bug fixes to make animations smoother and the app experience
    better.

--- Void/VoidViewController.swift
+++ Void/VoidViewController.swift
@@ -12,8 +12,10 @@ class VoidViewController: UIViewController {
     
     // Animation Global Variables
     
+    @IBOutlet weak var nextViewButton: UIButton!
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var pressAnywhereLabel: UILabel!
     @IBOutlet weak var blackHoleAnimateImageView: UIImageView!
-    @IBOutlet weak var triangleAnimateImageView: UIImageView!
     var triangleAnimateImageCount: Int = 1
     var animationCounter: Int = 1
     var animationTimeKeeper: Timer!
@@ -22,39 +24,42 @@ class VoidViewController: UIViewController {
     
     var quoteLibrary: [String] = ["All our dreams can come true if we have the courage to pursue them. - Walt Disney",
         "Someone is sitting in the shade today because someone planted a tree a long time ago. - Warren Buffett",
-        "You only live once, but if you do it right, once is enough. - Mae West",
-        "The difference between winning and losing is most often not quitting. - Walt Disney",
-        "I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison",
-        "You miss 100 percent of the shots you don't take. - Wayne Gretzky",
-        "You can't please everyone, and you can't make everyone like you. - Katie Couric",
-        "Don't worry about failure; you only have to be right once. - Drew Houston",
-        "Everything you can imagine is real. - Pablo Picasso",
-        "Be yourself. Everyone else is already taken. - Oscar Wilde",
-        "If you don’t build your dream, someone else will hire you to help them build theirs. - Dhirubhai Ambani",
-        "The best time to plant a tree was 20 years ago. The second best time is now. - Chinese Proverb",
-        "Everything you’ve ever wanted is on the other side of fear. - George Addair",
-        "In order to succeed, your desire for success should be greater than your fear of failure. - Bill Cosby",
-        "Screw it, let’s do it! - Richard Branson",
-        "Your time is limited, so don’t waste it living someone else’s life. – Steve Jobs",
-        "The Way Get Started Is To Quit Talking And Begin Doing. - Walt Disney",
-        "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.  – Robert Frost",
-        "You can never cross the ocean until you have the courage to lose sight of the shore. – Christopher Columbus",
-        "The only person you are destined to become is the person you decide to be. – Ralph Waldo Emerson",
-        "Believe you can and you’re halfway there. – Theodore Roosevelt",
+        "You only live once, but if you do it right, once is enough. - Mae West.",
+        "The difference between winning and losing is most often not quitting. - Walt Disney.",
+        "I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison.",
+        "You miss 100 percent of the shots you don't take. - Wayne Gretzky.",
+        "You can't please everyone, and you can't make everyone like you. - Katie Couric.",
+        "Don't worry about failure; you only have to be right once. - Drew Houston.",
+        "Everything you can imagine is real. - Pablo Picasso.",
+        "Be yourself. Everyone else is already taken. - Oscar Wilde.",
+        "If you don’t build your dream, someone else will hire you to help them build theirs. - Dhirubhai Ambani.",
+        "The best time to plant a tree was 20 years ago. The second best time is now. - Chinese Proverb.",
+        "Everything you’ve ever wanted is on the other side of fear. - George Addair.",
+        "In order to succeed, your desire for success should be greater than your fear of failure. - Bill Cosby.",
+        "Screw it, let’s do it! - Richard Branson.",
+        "Your time is limited, so don’t waste it living someone else’s life. – Steve Jobs.",
+        "The Way Get Started Is To Quit Talking And Begin Doing. - Walt Disney.",
+        "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.  – Robert Frost.",
+        "You can never cross the ocean until you have the courage to lose sight of the shore. – Christopher Columbus.",
+        "The only person you are destined to become is the person you decide to be. – Ralph Waldo Emerson.",
+        "Believe you can and you’re halfway there. – Theodore Roosevelt.",
         "Our lives begin to end the day we become silent about things that matter. – Martin Luther King Jr.",
-        "The only way to do great work is to love what you do. – Steve Jobs",
-        "Nothing is impossible, the word itself says, “I’m possible!” – Audrey Hepburn"]
+        "The only way to do great work is to love what you do. – Steve Jobs.",
+        "Nothing is impossible, the word itself says, “I’m possible!” – Audrey Hepburn."]
     var currentQuote: String!
     var quoteTimer: String!
+    var letterCounter: String.Index!
     
     func animateView() {
-        if (animationCounter == 1) {
-            animateTriangle()
-            animationCounter = animationCounter + 1
-        }
+        animationCounter = animationCounter + 1
         if (animationCounter == 2) {
             animateBlackHole()
-            animationCounter = animationCounter + 1
+        }
+        if (animationCounter == 5) {
+            blackHoleAnimateImageView.stopAnimating()
+            blackHoleAnimateImageView.image = #imageLiteral(resourceName: "Background Starry")
+            animationTimeKeeper.invalidate()
+            animationTimeKeeper = Timer.scheduledTimer(timeInterval: 0.025, target: self, selector: #selector(self.quoteAppear), userInfo: nil, repeats: true)
         }
     }
     
@@ -68,48 +73,35 @@ class VoidViewController: UIViewController {
             blackHoleAnimateImageArray.append(image!)
         }
         
+        blackHoleAnimateImageArray.append(#imageLiteral(resourceName: "Background Starry"))
+        
         blackHoleAnimateImageView.animationImages = blackHoleAnimateImageArray
-        blackHoleAnimateImageView.animationDuration = 2
+        blackHoleAnimateImageView.animationDuration = 2.6
         blackHoleAnimateImageView.startAnimating()
-        triangleAnimateImageView.stopAnimating()
     }
     
-    func animateTriangle() {
-        
-        var triangleAnimateImageArray = [UIImage]()
-        
-        for pictureNumber in 1...19
-        {
-            let imageName : String = "Triangle-Animate-\(pictureNumber).png"
-            let image  = UIImage(named:imageName)
-            triangleAnimateImageArray.append(image!)
-        }
-        
-        triangleAnimateImageView.animationImages = triangleAnimateImageArray
-        triangleAnimateImageView.animationDuration = 0.3333333333
-        triangleAnimateImageView.startAnimating()
-    }
     
     func quoteAppear() {
-//        if (letterCounter == quoteKeeper.endIndex) {
-//            letterAppearer.invalidate()
-//            pressAnywhereLabel.text = "Touch Anywhere To Continue"
-//            nextViewButton.isEnabled = true
-//            return
-//        }
-//        quoteLabel.text = quoteLabel.text! + String(quoteKeeper[letterCounter])
-//        letterCounter = quoteKeeper.index(after: letterCounter)
+        if (letterCounter == currentQuote.endIndex) {
+            animationTimeKeeper.invalidate()
+            pressAnywhereLabel.text = "Touch Anywhere to Continue"
+            nextViewButton.isEnabled = true
+            return
+        }
+        quoteLabel.text = quoteLabel.text! + String(currentQuote[letterCounter])
+        letterCounter = currentQuote.index(after: letterCounter)
     }
     
     func randomQuoteAssign() {
         let index: Int! = Int(arc4random()) % quoteLibrary.count
         currentQuote = quoteLibrary[index]
+        letterCounter = currentQuote.startIndex
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.isNavigationBarHidden = true
+        randomQuoteAssign()
         animationTimeKeeper = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.animateView), userInfo: nil, repeats: true)
-        animateTriangle()
     }
 
     override func viewDidLoad() {
